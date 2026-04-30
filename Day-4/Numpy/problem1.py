4ff527388eccfe1b75dc52d61c81c0174b5cd65d
Soham Jogdand <sohamjogdand355@gmail.com>
2026-04-30 15:14:42 +0530

    Day-3: Numpy Problem Practice

---
+++ Day-4/Numpy/problem1.py
@@ -0,0 +1,15 @@
+'''
+Create a NumPy array and:
+Find sum
+Mean
+Max
+'''
+
+import numpy as np
+
+arr = np.array([50,70,56,84,32])
+
+print("Array:", arr)
+print("Sum of array: ", np.sum(arr))
+print("mean of array: ", np.mean(arr))
+print("max of array: ", np.max(arr))
